b3e354035ceca44958d2492373d486d125547152
Tiaan Boshoff <tiaanboshoff02@gmail.com>
2023-06-09 16:34:24 -0700

    added code for setting up muse stream

---
+++ src/data_collection/lsl_streams/museSetup.py
@@ -0,0 +1,36 @@
+"""
+Setup the Muse device to begin streaming data to LSL.
+
+If data is not already streaming, the Bluemuse app will be launched and the
+user must manually connect the Muse and click on "Start Streaming"
+"""
+import subprocess
+from pylsl import StreamInfo, StreamInlet, resolve_stream, resolve_bypred
+
+# *signals: the signals to obtain from the muse. Any combination of "EEG",
+# "PPG", "Accelerometer", "Gyroscope". If unspecified, obtains all of these.
+def main(*signals):
+
+    validSignals = ("EEG", "PPG", "Accelerometer", "Gyroscope")
+    if len(signals) > 0:
+        if not all(signal in validSignals for signal in signals):
+            raise Exception("Invalid signal.")
+        targetSignals = signals
+    else:
+        targetSignals = validSignals
+
+    streams = resolve_stream()
+
+    # Check whether any desired signals are not yet streaming
+    if not all(stream.type() in targetSignals for stream in streams):
+        # Start bluemuse and enable desired signals to stream
+        for signal in validSignals:
+            key = signal.lower() + "_enabled"
+            value = str(signal in targetSignals).lower()
+            subprocess.run(f'start bluemuse://setting?key={key}!value={value}',
+                shell=True)
+        
+        # Wait until all desired signals are streaming
+        pred = " or ".join(f"type='{x}'" for x in targetSignals)
+        streams = resolve_bypred(pred, minimum=len(targetSignals))
+
--- src/data_collection/startSession.py
+++ src/data_collection/startSession.py
@@ -1,1 +1,8 @@
-"" 
+"""
+Start a data collection session.
+""" 
+from pylsl import StreamInfo, StreamInlet, resolve_stream
+
+def main():
+    streams = resolve_stream()
+    
