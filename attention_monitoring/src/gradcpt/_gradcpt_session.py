f6fdad618a6ff8c6ffb0b9963233a4e437439b50
Tiaan Boshoff <tiaanboshoff02@gmail.com>
2023-08-13 13:47:39 -0700

    tried making gradcpt async

--- attention_monitoring/src/gradcpt/_gradcpt_session.py
+++ attention_monitoring/src/gradcpt/_gradcpt_session.py
@@ -1,10 +1,14 @@
 from abc import abstractmethod
+import aiofiles
+import asyncio
 import csv
 from io import StringIO
 import logging
 import os
+import shlex
 import subprocess
 from time import sleep
+from typing import Callable
 
 import matlab.engine
 import polars as pl
@@ -121,6 +125,115 @@ class GradCPTSession(StudySession):
             _log.debug("Connecting to the EEG")
             self.eeg.connect()
             self.eeg.startStreaming()
+            
+            #######
+            
+            async def startMuse(*signals):
+                validSignals = ["EEG", "PPG", "Accelerometer", "Gyroscope"]                
+                if len(signals) > 0:
+                    if not all(signal in validSignals for signal in signals):
+                        raise Exception("Invalid signal.")
+                    _signals = signals
+                else:
+                    _signals = validSignals
+                
+                async def execBluemuse(**kwargs):
+                    if len(kwargs) == 0:
+                        raise ValueError(
+                            "Must specify at least one key-value pair."
+                            )
+                    for k, v in kwargs.items():
+                        command = f'start bluemuse://setting?key={k}!value={v}'
+                        command = shlex.split(command)
+                        yield asyncio.create_subprocess_exec(command)
+                        
+                bluemuseOptions = {
+                    (s.lower() + "_enabled") : (str(s in _signals).lower())
+                    for s in validSignals
+                    }
+                bluemuseOptions.update(
+                    {
+                        "primary_timestamp_format" : "LSL_LOCAL_CLOCK_NATIVE"
+                    }
+                )
+                
+                procs = [proc async for proc in execBluemuse(bluemuseOptions)]       
+            
+            async def startMatlab() -> matlab.engine.MatlabEngine:
+                future = matlab.engine.start_matlab(background=True)
+                while not future.done():
+                    asyncio.wait(0.5)
+                return future.result()
+            
+            async def startLR() -> asyncio.Process:
+                _log.debug("Starting LabRecorder")
+                lrLogPath = os.path.join(self._DIR, "lab_recorder.log")
+                _log.debug("Writing LabRecorder output to file: %s", lrLogPath)
+                proc = await asyncio.create_subprocess_exec(
+                    os.path.realpath(CONFIG.path_to_LabRecorder),
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.STDOUT
+                    )
+                return proc
+            
+            async def startBlueMuse() -> None:
+                
+                
+            def runExperiment(eng) -> None:
+                # Intentionally blocking, give as much control to MATLAB as
+                # possible. Run MATLAB in background so we can save output to a
+                # file instead.
+                # TODO: should we save output to file directly in MATLAB instead?
+                _log.debug("Displaying stimuli using Psychtoolbox")
+                with StringIO() as matlabOut:
+                    p = eng.genpath(CONFIG.projectRoot)
+                    eng.addpath(p, nargout=0)
+                    future = eng.gradCPT(
+                        self._info["info_file"],
+                        'verbose', CONFIG.verbose,
+                        'streamMarkersToLSL', CONFIG.stream_markers_to_lsl,
+                        'recordLSL', CONFIG.record_lsl,
+                        'tcpAddress', CONFIG.tcp_address,
+                        'tcpPort', CONFIG.tcp_port,
+                        stdout=matlabOut,
+                        stderr=matlabOut,
+                        background=True
+                        )
+                    
+                    matlabLog = os.path.join(self._DIR, "matlab.log")
+                    _log.debug("Writing MATLAB output to file: %s", matlabLog)
+                    # TODO: is below with block allowed? this is naive code, does it work?
+                    with open(matlabLog, "a") as f:
+                        while not future.done():
+                            f.write(matlabOut.readline())
+                        _log.debug("Done running experiment in MATLAB")
+                        f.write(matlabOut.getvalue())
+            
+                            
+            async def writeStreamToFile(
+                stream: asyncio.StreamReader, 
+                filePath: str, 
+                callback: [None | Callable[[str], str]] = None
+                ) -> None:
+                _callback = callback if callback is not None else lambda x: x
+                with aiofiles.open(filePath, "a") as f:
+                    while not stream.at_eof():
+                        data = await stream.readLine()
+                        line = data.decode('ascii')
+                        line = _callback(line)
+                        await f.write(line)
+                        
+                        
+                
+            
+                
+                
+            
+            
+            
+            
+            
+            #######
             
             # Start LabRecorder
             if CONFIG.path_to_LabRecorder != "":
